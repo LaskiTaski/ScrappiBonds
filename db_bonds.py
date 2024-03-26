986929c9c0b96d33f416a62e2b2a654e3a227528
Tim <dgj.vap.11@mail.ru>
2024-03-27 01:45:25 +0300

    UPDATE: DB_bonds - Изменены имена переменных на более короткие
    Сформирована новая таблица

--- db_bonds.py
+++ db_bonds.py
@@ -14,17 +14,17 @@ def create_table_all_bonds():
         sqlite_connection = sqlite3.connect('SMART-Bonds.db')
         sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS All_Bonds (
                                     URL TEXT UNIQUE,
-                                    Название TEXT,
-                                    Котировка_облигации REAL DEFAULT "Информация отсутствует",
-                                    Доходность_к_погашению REAL DEFAULT "Информация отсутствует",
-                                    Доходность_купона_к_рынку REAL DEFAULT "Информация отсутствует",
-                                    Доходность_купона_к_номиналу REAL DEFAULT "Информация отсутствует",
-                                    Частота_купона INTEGER DEFAULT "Информация отсутствует",
-                                    Дата_погашения DATETIME DEFAULT "Информация отсутствует",
-                                    Дней_до_погашения INTEGER DEFAULT "Информация отсутствует",
-                                    ISIN TEXT DEFAULT "Информация отсутствует",
-                                    Код_Бумаги TEXT DEFAULT "Информация отсутствует",
-                                    Только_для_квалов TEXT DEFAULT "Информация отсутствует"
+                                    NAME TEXT,
+                                    Quoting REAL,
+                                    Repayment REAL,
+                                    Market REAL,
+                                    Nominal REAL,
+                                    Frequency INTEGER,
+                                    Date DATETIME,
+                                    Days INTEGER,
+                                    ISIN TEXT,
+                                    Code TEXT,
+                                    Qualification TEXT
                                     );'''
 
         cursor = sqlite_connection.cursor()
@@ -43,21 +43,21 @@ def insert_change_into_table(information_bonds):
         cursor = sqlite_connection.cursor()
 
         url = information_bonds[0]
-        cursor.execute("SELECT * FROM All_Bonds WHERE url=?", (url,))
+        cursor.execute("SELECT * FROM All_Bonds WHERE URL=?", (url,))
         result = cursor.fetchone()
 
         if result:
-            sqlite_insert_change_with_param = """UPDATE All_Bonds SET Название=?, Котировка_облигации=?, 
-            Доходность_к_погашению=?, Доходность_купона_к_рынку=?, Доходность_купона_к_номиналу=?, Частота_купона=?,
-            Дата_погашения=?, Дней_до_погашения=?, ISIN=?, Код_Бумаги=?, Только_для_квалов=? WHERE URL=?"""
+            sqlite_insert_change_with_param = """UPDATE All_Bonds SET NAME=?, Quoting=?, Repayment=?, 
+                                                Market=?, Nominal=?, Frequency=?, Date=?, Days=?, ISIN=?,
+                                                Code=?, Qualification=? WHERE URL=?"""
             data_tuple = tuple(information_bonds[1::]) + (url,)
             print('UPDATE----------UPDATE----------UPDATE----------UPDATE----------UPDATE----------UPDATE----------UPDATE')
         else:
             sqlite_insert_change_with_param = """INSERT INTO All_Bonds
-                                  (URL, Название, Котировка_облигации, Доходность_к_погашению, 
-                                  Доходность_купона_к_рынку, Доходность_купона_к_номиналу, 
-                                  Частота_купона, Дата_погашения, Дней_до_погашения, ISIN, 
-                                  Код_Бумаги, Только_для_квалов)
+                                  (URL, NAME, Quoting, Repayment, 
+                                  Market, Nominal, 
+                                  Frequency, Date, Days, ISIN, 
+                                  Code, Qualification)
                                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
             data_tuple = tuple(information_bonds)
             print('INSERT----------INSERT----------INSERT----------INSERT----------INSERT----------INSERT----------INSERT')
